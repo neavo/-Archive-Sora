29faef99414c7591441f41c01207d609dde54b3d
Neavo <neavo7@gmail.com>
2011-08-26 00:32:19 +0800

    CastBy样式调整

--- Buff/CastBy.lua
+++ Buff/CastBy.lua
@@ -1,4 +1,4 @@
-local function addAuraSource(self, func, unit, index, filter)
+ï»¿local function addAuraSource(self, func, unit, index, filter)
    local caster = select(8, func(unit, index, filter))
    if caster then
       self:AddLine(" ")
@@ -11,20 +11,20 @@ local function addAuraSource(self, func, unit, index, filter)
       else
          if caster == 'pet' or caster == 'vehicle' then
             color = colors[select(2,UnitClass('player'))]
-            castername = format('|cff%02X%02X%02X%s|r%s%s', color.r*255, color.g*255, color.b*255, UnitName('player'), '|cffC0C0C0µÄ|r', UnitName(caster))
+            castername = format('|cff%02X%02X%02X%s|r%s%s', color.r*255, color.g*255, color.b*255, UnitName('player'), '|cffC0C0C0çš„|r', UnitName(caster))
          else
             partypet = caster:match('^partypet(%d+)$')
             raidpet = caster:match('^raidpet(%d+)$')
             if partypet then
                color = colors[select(2,UnitClass('party'..partypet))]
-               castername = format('|cff%02X%02X%02X%s|r%s%s' , color.r*255, color.g*255, color.b*255, UnitName("party"..partypet), '|cffC0C0C0µÄ|r', UnitName(caster))
+               castername = format('|cff%02X%02X%02X%s|r%s%s' , color.r*255, color.g*255, color.b*255, UnitName("party"..partypet), '|cffC0C0C0çš„|r', UnitName(caster))
             elseif raidpet then
                color = colors[select(2,UnitClass('party'..raidpet))]
-               castername = format('|cff%02X%02X%02X%s|r%s%s' , color.r*255, color.g*255, color.b*255, UnitName('raid'..raidpet), '|cffC0C0C0µÄ|r', UnitName(caster))
+               castername = format('|cff%02X%02X%02X%s|r%s%s' , color.r*255, color.g*255, color.b*255, UnitName('raid'..raidpet), '|cffC0C0C0çš„|r', UnitName(caster))
             end
          end
       end
-      self:AddDoubleLine('|cffC0C0C0Cast by:|r', castername)
+      self:AddDoubleLine('Cast byï¼š', castername)
       self:Show()
    end
 end
