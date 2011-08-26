806308461fab50f919e0d1b68d74d65614df5fe4
Neavo <neavo7@gmail.com>
2011-08-26 15:59:12 +0800

    代码调整

--- Chat/AutoSet.lua
+++ Chat/AutoSet.lua
@@ -14,15 +14,12 @@ if not AutoSet then return end
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function()
-	for i = 1, NUM_CHAT_WINDOWS do
-		local ChatFrame = _G["ChatFrame"..i]
-		ChatFrame:SetFont(cfg.Font, 12, "THINOUTLINE") 
-		ChatFrame:ClearAllPoints()
-		ChatFrame:SetPoint("BOTTOMLEFT",UIParent,"BOTTOMLEFT",10,30)
-		ChatFrame:SetWidth(360)
-		ChatFrame:SetHeight(100)
-		ChatFrame:SetFrameLevel(8)
-		FCF_SetLocked(ChatFrame, true)
-	end
+	_G["ChatFrame1"]:SetFont(cfg.Font, 12, "THINOUTLINE") 
+	_G["ChatFrame1"]:ClearAllPoints()
+	_G["ChatFrame1"]:SetPoint("BOTTOMLEFT",UIParent,"BOTTOMLEFT",10,30)
+	_G["ChatFrame1"]:SetWidth(360)
+	_G["ChatFrame1"]:SetHeight(100)
+	_G["ChatFrame1"]:SetFrameLevel(8)
+	FCF_SetLocked(_G["ChatFrame1"], true)
 	f:UnregisterEvent("PLAYER_LOGIN")
 end)
