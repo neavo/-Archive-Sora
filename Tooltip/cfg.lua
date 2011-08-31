aac83aae06feb7c394780a20aa4f7a6c60de9384
Neavo <neavo7@gmail.com>
2011-08-31 19:50:09 +0800

    添加Tooltip模块

--- Tooltip/cfg.lua
+++ Tooltip/cfg.lua
@@ -15,7 +15,7 @@ cfg.GlowTex = Media.."glowTex"
 cfg.colorborderClass = true											-- 边框职业着色
 cfg.Cursor = true													-- 提示框体跟随鼠标
 	cfg.Position = { "BOTTOMRIGHT", nil, "BOTTOMRIGHT", -50, 200 }	-- 如果不跟随鼠标,那提示框体的位置
-cfg.HideInCombat = true -- 进入战斗自动隐藏提示框体
+cfg.HideInCombat = true 											-- 进入战斗自动隐藏提示框体
 
 
 ----------------
