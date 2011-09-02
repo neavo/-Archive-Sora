a5dfb052c6727f10f73f5e16e4e1b73c5da39177
Neavo <neavo7@gmail.com>
2011-09-02 10:03:47 +0800

    信息条模块

---
+++ Topbar/cfg.lua
@@ -0,0 +1,20 @@
+﻿
+
+------------
+--  设置  --
+------------
+
+local cfg = CreateFrame("Frame")
+local Media = "InterFace\\AddOns\\Sora's\\Topbar\\Media\\"
+cfg.Font = "Fonts\\ZYKai_T.ttf"
+cfg.GlowTex = Media.."glowTex"
+cfg.Statusbar = Media.."statusbar"
+
+
+----------------
+--  命名空间  --
+----------------
+
+local _, SR = ...
+SR.TopBarConfig = cfg
+
