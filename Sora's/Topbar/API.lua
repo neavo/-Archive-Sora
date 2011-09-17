aeea1dfbe2e67ef45f4da49149635692734d8137
Neavo <neavo7@gmail.com>
2011-09-17 14:31:34 +0800

    强化了顶部信息条的功能

---
+++ Sora's/Topbar/API.lua
@@ -0,0 +1,318 @@
+﻿----------------
+--  命名空间  --
+----------------
+
+local _, SR = ...
+local cfg = SR.TopBarConfig
+
+
+
+
+--API and Engine by Tukz and Elv
+local addon, engine = ...
+engine[1] = {} -- E, functions, constants
+engine[2] = {} -- C, config
+engine[3] = {} -- L, localization
+engine[4] = {} -- DB, database, post config load
+
+RayUI = engine 
+
+local R = unpack(select(2, ...))
+_, R.myclass = UnitClass("player")
+R.level = UnitLevel("player")
+R.myname = UnitName("player")
+R.myrealm = GetRealmName()
+RoleUpdater = CreateFrame("Frame")
+
+local function CheckRole(self, event, unit)
+	local tree = GetPrimaryTalentTree()
+	local resilience
+	local resilperc = GetCombatRatingBonus(COMBAT_RATING_RESILIENCE_PLAYER_DAMAGE_TAKEN)
+	if resilperc > GetDodgeChance() and resilperc > GetParryChance() then
+		resilience = true
+	else
+		resilience = false
+	end
+	if ((R.myclass == "PALADIN" and tree == 2) or 
+	(R.myclass == "WARRIOR" and tree == 3) or 
+	(R.myclass == "DEATHKNIGHT" and tree == 1)) and
+	resilience == false or
+	(R.myclass == "DRUID" and tree == 2 and GetBonusBarOffset() == 3) then
+		R.Role = "Tank"
+	else
+		local playerint = select(2, UnitStat("player", 4))
+		local playeragi	= select(2, UnitStat("player", 2))
+		local base, posBuff, negBuff = UnitAttackPower("player");
+		local playerap = base + posBuff + negBuff;
+
+		if (((playerap > playerint) or (playeragi > playerint)) and not (R.myclass == "SHAMAN" and tree ~= 1 and tree ~= 3) and not (UnitBuff("player", GetSpellInfo(24858)) or UnitBuff("player", GetSpellInfo(65139)))) or R.myclass == "ROGUE" or R.myclass == "HUNTER" or (R.myclass == "SHAMAN" and tree == 2) then
+			R.Role = "Melee"
+		else
+			R.Role = "Caster"
+		end
+	end
+end	
+RoleUpdater:RegisterEvent("PLAYER_ENTERING_WORLD")
+RoleUpdater:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+RoleUpdater:RegisterEvent("PLAYER_TALENT_UPDATE")
+RoleUpdater:RegisterEvent("CHARACTER_POINTS_CHANGED")
+RoleUpdater:RegisterEvent("UNIT_INVENTORY_CHANGED")
+RoleUpdater:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
+RoleUpdater:SetScript("OnEvent", CheckRole)
+CheckRole()
+
+local mult = 768/string.match(GetCVar("gxResolution"), "%d+x(%d+)")/UIParent:GetScale()
+local function scale(x)
+	return mult*math.floor(x/mult+.5)
+end
+
+function R.RGBToHex(r, g, b)
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format("|cff%02x%02x%02x", r*255, g*255, b*255)
+end
+
+function R.ShortValue(v)
+	if v >= 1e6 then
+		return ("%.1fm"):format(v / 1e6):gsub("%.?0+([km])$", "%1")
+	elseif v >= 1e3 or v <= -1e3 then
+		return ("%.1fk"):format(v / 1e3):gsub("%.?0+([km])$", "%1")
+	else
+		return v
+	end
+end
+
+local function Size(frame, width, height)
+	frame:SetSize(scale(width), scale(height or width))
+end
+
+local function Width(frame, width)
+	frame:SetWidth(scale(width))
+end
+
+local function Height(frame, height)
+	frame:SetHeight(scale(height))
+end
+
+local function Point(obj, arg1, arg2, arg3, arg4, arg5)
+	-- anyone has a more elegant way for this?
+	if type(arg1)=="number" then arg1 = scale(arg1) end
+	if type(arg2)=="number" then arg2 = scale(arg2) end
+	if type(arg3)=="number" then arg3 = scale(arg3) end
+	if type(arg4)=="number" then arg4 = scale(arg4) end
+	if type(arg5)=="number" then arg5 = scale(arg5) end
+
+	obj:SetPoint(arg1, arg2, arg3, arg4, arg5)
+end
+
+local function CreateShadow(f, t)
+	if f.shadow then return end
+	
+	borderr, borderg, borderb, bordera = 0, 0, 0, 1
+	backdropr, backdropg, backdropb, backdropa = 05,.05,.05, .9
+	
+	local shadow = CreateFrame("Frame", nil, f)
+	shadow:SetFrameLevel(1)
+	shadow:SetFrameStrata(f:GetFrameStrata())
+	shadow:Point("TOPLEFT", -3, 3)
+	shadow:Point("BOTTOMRIGHT", 3, -3)
+	shadow:Point("TOPRIGHT", 3, 3)
+	shadow:Point("BOTTOMRIGHT", 3, -3)
+	shadow:SetBackdrop({
+		edgeFile = cfg.GlowTex, 
+		edgeSize = scale(5),
+		insets = { left = scale(4), right = scale(4), top = scale(4), bottom = scale(4) }
+	})
+	shadow:SetBackdropColor( backdropr, backdropg, backdropb, backdropa )
+	shadow:SetBackdropBorderColor( borderr, borderg, borderb, bordera )
+	f.shadow = shadow
+end
+
+local function SetTemplate(f, t, texture)
+	f:SetBackdrop({
+	  bgFile = cfg.Statusbar,
+	})
+	f:SetBackdropColor( .05,.05,.05, .9)
+	f:SetBackdropBorderColor(0, 0, 0, 1)
+end
+
+local function StyleButton(b, c) 
+	local name = b:GetName()
+
+	local button          = _G[name]
+	local icon            = _G[name.."Icon"]
+	local count           = _G[name.."Count"]
+	local border          = _G[name.."Border"]
+	local hotkey          = _G[name.."HotKey"]
+	local cooldown        = _G[name.."Cooldown"]
+	local nametext        = _G[name.."Name"]
+	local flash           = _G[name.."Flash"]
+	local normaltexture   = _G[name.."NormalTexture"]
+	local icontexture     = _G[name.."IconTexture"]
+
+	local hover = b:CreateTexture("frame", nil, self) -- hover
+	hover:SetTexture(1,1,1,0.3)
+	hover:SetHeight(button:GetHeight())
+	hover:SetWidth(button:GetWidth())
+	hover:Point("TOPLEFT",button, 2 , -2)
+	hover:Point("BOTTOMRIGHT",button, -2, 2)
+	button:SetHighlightTexture(hover)
+
+	local pushed = b:CreateTexture("frame", nil, self) -- pushed
+	pushed:SetTexture(0.9,0.8,0.1,0.3)
+	pushed:SetHeight(button:GetHeight())
+	pushed:SetWidth(button:GetWidth())
+	pushed:Point("TOPLEFT",button,2,-2)
+	pushed:Point("BOTTOMRIGHT",button,-2,2)
+	button:SetPushedTexture(pushed)
+
+	if cooldown then
+		cooldown:ClearAllPoints()
+		cooldown:Point("TOPLEFT",button,2,-2)
+		cooldown:Point("BOTTOMRIGHT",button,-2,2)
+	end
+
+	if c then
+		local checked = b:CreateTexture("frame", nil, self) -- checked
+		checked:SetTexture(23/255,132/255,209/255,0.3)
+		checked:SetHeight(button:GetHeight())
+		checked:SetWidth(button:GetWidth())
+		checked:Point("TOPLEFT",button,2,-2)
+		checked:Point("BOTTOMRIGHT",button,-2,2)
+		checked:SetAlpha(0.3)
+		button:SetCheckedTexture(checked)
+	end
+end
+
+local function CreatePanel(f, t, w, h, a1, p, a2, x, y)
+	local sh = scale(h)
+	local sw = scale(w)
+	f:SetFrameLevel(1)
+	f:SetHeight(sh)
+	f:SetWidth(sw)
+	f:SetFrameStrata("BACKGROUND")
+	f:SetPoint(a1, p, a2, scale(x), scale(y))
+	if t ~= "Transparent" then
+		f:SetTemplate(t)
+	end
+	f:CreateShadow(t)
+end
+
+local function Kill(object)
+	if object.IsProtected then 
+		if object:IsProtected() then
+			error("Attempted to kill a protected object: <"..object:GetName()..">")
+		end
+	end
+	if object.UnregisterAllEvents then
+		object:UnregisterAllEvents()
+	end
+	object.Show = noop
+	object:Hide()
+end
+
+local function FadeIn(f)
+	UIFrameFadeIn(f, .4, f:GetAlpha(), 1)
+end
+	
+local function FadeOut(f)
+	UIFrameFadeOut(f, .4, f:GetAlpha(), 0)
+end
+
+local function addapi(object)
+	local mt = getmetatable(object).__index
+	if not object.Size then mt.Size = Size end
+	if not object.Point then mt.Point = Point end
+	if not object.SetTemplate then mt.SetTemplate = SetTemplate end
+	if not object.CreatePanel then mt.CreatePanel = CreatePanel end
+	if not object.CreateShadow then mt.CreateShadow = CreateShadow end
+	if not object.Kill then mt.Kill = Kill end
+	if not object.StyleButton then mt.StyleButton = StyleButton end
+	if not object.Width then mt.Width = Width end
+	if not object.Height then mt.Height = Height end
+	if not object.FadeIn then mt.FadeIn = FadeIn end
+	if not object.FadeOut then mt.FadeOut = FadeOut end
+end
+local handled = {["Frame"] = true}
+local object = CreateFrame("Frame")
+addapi(object)
+addapi(object:CreateTexture())
+addapi(object:CreateFontString())
+object = EnumerateFrames()
+while object do
+	if not handled[object:GetObjectType()] then
+		addapi(object)
+		handled[object:GetObjectType()] = true
+	end
+
+	object = EnumerateFrames(object)
+end
+
+--实名好友弹窗位置
+BNToastFrame:HookScript("OnShow", function(self)
+	self:ClearAllPoints()
+	self:SetPoint("BOTTOMLEFT", ChatFrame1Tab, "TOPLEFT", 0, 0)
+end)
+
+local AchievementHolder = CreateFrame("Frame", "AchievementHolder", UIParent)
+AchievementHolder:SetWidth(180)
+AchievementHolder:SetHeight(20)
+AchievementHolder:SetPoint("CENTER", UIParent, "CENTER", 0, 170)
+
+local pos = "TOP"
+
+function AchievementMove(self, event, ...)
+	local previousFrame
+	for i=1, MAX_ACHIEVEMENT_ALERTS do
+		local aFrame = _G["AchievementAlertFrame"..i]
+		if ( aFrame ) then
+			aFrame:ClearAllPoints()
+			if pos == "TOP" then
+				if ( previousFrame and previousFrame:IsShown() ) then
+					aFrame:SetPoint("TOP", previousFrame, "BOTTOM", 0, -10)
+				else
+					aFrame:SetPoint("TOP", AchievementHolder, "BOTTOM")
+				end
+			else
+				if ( previousFrame and previousFrame:IsShown() ) then
+					aFrame:SetPoint("BOTTOM", previousFrame, "TOP", 0, 10)
+				else
+					aFrame:SetPoint("BOTTOM", AchievementHolder, "TOP")	
+				end			
+			end
+			
+			previousFrame = aFrame
+		end
+	end
+	
+end
+
+hooksecurefunc("AchievementAlertFrame_FixAnchors", AchievementMove)
+
+hooksecurefunc("DungeonCompletionAlertFrame_FixAnchors", function()
+	for i=MAX_ACHIEVEMENT_ALERTS, 1, -1 do
+		local aFrame = _G["AchievementAlertFrame"..i]
+		if ( aFrame and aFrame:IsShown() ) then
+			DungeonCompletionAlertFrame1:ClearAllPoints()
+			if pos == "TOP" then
+				DungeonCompletionAlertFrame1:SetPoint("TOP", aFrame, "BOTTOM", 0, -10)
+			else
+				DungeonCompletionAlertFrame1:SetPoint("BOTTOM", aFrame, "TOP", 0, 10)
+			end
+			
+			return
+		end
+		
+		DungeonCompletionAlertFrame1:ClearAllPoints()	
+		if pos == "TOP" then
+			DungeonCompletionAlertFrame1:SetPoint("TOP", AchievementHolder, "BOTTOM")
+		else
+			DungeonCompletionAlertFrame1:SetPoint("BOTTOM", AchievementHolder, "TOP")
+		end
+	end
+end)
+
+local achieveframe = CreateFrame("Frame")
+achieveframe:RegisterEvent("ACHIEVEMENT_EARNED")
+achieveframe:SetScript("OnEvent", function(self, event, ...) AchievementMove(self, event, ...) end)
