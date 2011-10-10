dbc052f103afb75ae352e9e856b8f301796003b3
Neavo <neavo7@gmail.com>
2011-10-10 13:34:43 +0800

    代码优化

--- Sora's MiniMap/MiniMapPanels.lua
+++ Sora's MiniMap/MiniMapPanels.lua
@@ -2,142 +2,248 @@
 --  命名空间  --
 ----------------
 
-local _, SR = ...
-local cfg = SR.MapConfig
+local _, ns = ...
+local cfg = ns.cfg
 
-local NBBVisible
+-- InitMiniMapTracking
+local function InitMiniMapTracking(TopFrame)
+	MiniMapTrackingBackground:Hide()
+	MiniMapTrackingButton:SetAlpha(0)
+	MiniMapTracking:SetParent(TopFrame)
+	MiniMapTracking:ClearAllPoints()
+	MiniMapTracking:SetFrameStrata("HIGH")
+	MiniMapTracking:SetFrameLevel("0")
+	MiniMapTracking:SetPoint("LEFT", 3, 0)
+	MiniMapTracking:SetScale(0.7)
+end
 
--- 顶部(区域信息)
-local TMFrame = CreateFrame("Frame", nil, Minimap)
-TMFrame:SetHeight(16)
-TMFrame:SetWidth(Minimap:GetWidth())
-TMFrame:SetPoint("BOTTOM", Minimap, "TOP", 0, 3)
-TMFrame:SetBackdrop( { 
-	edgeFile = cfg.Solid, edgeSize = 1, 
-})
-TMFrame:SetBackdropBorderColor(0, 0, 0, 1)
-TMFrame:SetAlpha(0.2)
-TMFrame:SetScript("OnEnter", function(self)
-	self:SetAlpha(1)
-end)
-TMFrame:SetScript("OnLeave", function(self)
-	self:SetAlpha(0.2)
-end)
-TMFrame.Text = TMFrame:CreateFontString(nil, "OVERLAY")
-TMFrame.Text:SetPoint("CENTER", TMFrame, "CENTER", 0, 0)
-TMFrame.Text:SetFont(cfg.Font, 10, "THINOUTLINE")
+-- BuildTopFrame
+local function BuildTopFrame()
+	local TopFrame = CreateFrame("Frame", nil, Minimap)
+	TopFrame:SetHeight(16)
+	TopFrame:SetWidth(Minimap:GetWidth())
+	TopFrame:SetPoint("BOTTOM", Minimap, "TOP", 0, 3)
+	TopFrame:SetBackdrop( { 
+		edgeFile = cfg.Solid, edgeSize = 1, 
+	})
+	TopFrame:SetBackdropBorderColor(0, 0, 0, 1)
+	TopFrame:SetAlpha(0.2)
+	TopFrame:SetScript("OnEnter", function(self)
+		self:SetAlpha(1)
+	end)
+	TopFrame:SetScript("OnLeave", function(self)
+		self:SetAlpha(0.2)
+	end)	
+	TopFrame.Text = TopFrame:CreateFontString(nil, "OVERLAY")
+	TopFrame.Text:SetPoint("CENTER", TopFrame, "CENTER", 0, 0)
+	TopFrame.Text:SetFont(cfg.Font, 10, "THINOUTLINE")
+	TopFrame.Timer = 0
+	TopFrame:SetScript("OnUpdate", function(self, elapsed)
+		self.Timer = self.Timer + elapsed
+		if self.Timer > 1 then
+			self.Timer = 0
+			self.Text:SetText(GetMinimapZoneText())
+		end
+	end)
+	
+	InitMiniMapTracking(TopFrame)
+end
 
-MiniMapTrackingBackground:Hide()
-MiniMapTrackingButton:SetAlpha(0)
-MiniMapTracking:SetParent(TMFrame)
-MiniMapTracking:ClearAllPoints()
-MiniMapTracking:SetFrameStrata("HIGH")
-MiniMapTracking:SetFrameLevel("0")
-MiniMapTracking:SetPoint("LEFT", TMFrame, "LEFT" , 3, 0)
-MiniMapTracking:SetScale(0.7)
+-- BuildBottomRightFrame
+local function BuildBottomRightFrame(BottomFrame)
+	BottomFrame.Right = CreateFrame("Button", nil, BottomFrame)
+	BottomFrame.Right:SetSize(16, 16)
+	BottomFrame.Right:SetPoint("RIGHT")
+	BottomFrame.Right:SetScript("OnEnter", function(self)
+		BottomFrame:SetAlpha(1)
+		self.Text:SetTextColor(1, 0, 0)
+	end)
+	BottomFrame.Right:SetScript("OnLeave", function(self)
+		BottomFrame:SetAlpha(0.2)
+		self.Text:SetTextColor(1, 1, 1)
+	end)
+	BottomFrame.Right:SetScript("OnMouseDown", function(self, button, ...)
+			if NBB:IsShown() then
+				NBB:Hide()
+			else
+				NBB:Show()
+			end
+			PlaySound("igMiniMapOpen")
+	end)
+	BottomFrame.Right.Text = BottomFrame.Right:CreateFontString(nil, "OVERLAY")
+	BottomFrame.Right.Text:SetPoint("CENTER")
+	BottomFrame.Right.Text:SetFont(cfg.Font, 9, "THINOUTLINE")
+	BottomFrame.Right.Text:SetText("B")
+end
 
--- 底部中央(坐标)
-local BMFrame = CreateFrame("Frame", "BMFrame", Minimap)
-BMFrame:SetHeight(16)
-BMFrame:SetWidth(Minimap:GetWidth())
-BMFrame:SetPoint("TOP", Minimap, "BOTTOM", 0, -3)
-BMFrame:SetBackdrop({edgeFile = cfg.Solid, edgeSize = 1})
-BMFrame:SetBackdropBorderColor(0, 0, 0, 1)
-BMFrame:SetAlpha(0.2)
-BMFrame:SetScript("OnEnter", function(self)
-	self:SetAlpha(1)
-end)
-BMFrame:SetScript("OnLeave", function(self)
-	self:SetAlpha(0.2)
-end)
-BMFrame.Text = BMFrame:CreateFontString(nil, "OVERLAY")
-BMFrame.Text:SetPoint("CENTER", BMFrame, "CENTER", 0, 0)
-BMFrame.Text:SetFont(cfg.Font, 10, "THINOUTLINE")
-
--- 底部左边(团队菜单)
-local BLFrame = CreateFrame("Button", nil, BMFrame, "SecureActionButtonTemplate")
-BLFrame:SetSize(16, 16)
-BLFrame:SetAttribute("type", "macro")
-BLFrame:SetAttribute("macrotext1", "/click CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton")
-BLFrame:SetPoint("LEFT", BMFrame, "LEFT", 0, 0)
-BLFrame:SetScript("OnEnter", function(self)
-	BMFrame:SetAlpha(1)
-	BLFrame.Text:SetTextColor(1, 0, 0)
-end)
-BLFrame:SetScript("OnLeave", function(self)
-	BMFrame:SetAlpha(0.2)
-	BLFrame.Text:SetTextColor(1, 1, 1)
-end)
-BLFrame.Text = BLFrame:CreateFontString(nil, "OVERLAY")
-BLFrame.Text:SetPoint("CENTER", BLFrame, "CENTER", 0, 0)
-BLFrame.Text:SetFont(cfg.Font, 9, "THINOUTLINE")
-BLFrame.Text:SetText("G")
-BLFrame.RaidMenuFrame = CreateFrame("Frame", "RaidMenu", UIParent, "UIDropDownMenuTemplate")
-BLFrame:SetScript("OnMouseDown", function(self, button)
-	if button == "RightButton" then
-		EasyMenu({
-			{text = "就位确认", func = function() DoReadyCheck() end}, 
-			{text = "角色检查", func = function() InitiateRolePoll() end}, 
-			{text = "转化为团队", func = function() ConvertToRaid() end}, 
-			{text = "转化为小队", func = function() ConvertToParty() end}},
-		self.RaidMenuFrame, "cursor", 0, 0, "MENU", 2)
-	end
-	PlaySound("igMiniMapOpen")
-end)
-DropDownList1:SetClampedToScreen(true)
-
--- 底部右边(NBB)
-local BRFrame = CreateFrame("Frame", nil, BMFrame)
-BRFrame:SetHeight(16)
-BRFrame:SetWidth(16)
-BRFrame:SetPoint("RIGHT", BMFrame, "RIGHT", 0, 0)
-BRFrame:SetScript("OnEnter", function(self)
-	BMFrame:SetAlpha(1)
-	BRFrame.Text:SetTextColor(1, 0, 0)
-end)
-BRFrame:SetScript("OnLeave", function(self)
-	BMFrame:SetAlpha(0.2)
-	BRFrame.Text:SetTextColor(1, 1, 1)
-end)
-BRFrame:SetScript("OnMouseDown", function(self)
-		if NBBVisible then
-			NBB:Hide()
-		else
-			NBB:Show()
+-- BuildBottomLeftFrame
+local function BuildBottomLeftFrame(BottomFrame)
+	BottomFrame.Left = CreateFrame("Button", nil, BottomFrame)
+	BottomFrame.Left:SetSize(16, 16)
+	BottomFrame.Left:SetPoint("LEFT")
+	BottomFrame.Left:SetScript("OnEnter", function(self)
+		BottomFrame:SetAlpha(1)
+		self.Text:SetTextColor(1, 0, 0)
+	end)
+	BottomFrame.Left:SetScript("OnLeave", function(self)
+		BottomFrame:SetAlpha(0.2)
+		self.Text:SetTextColor(1, 1, 1)
+	end)
+	BottomFrame.Left.Text = BottomFrame.Left:CreateFontString(nil, "OVERLAY")
+	BottomFrame.Left.Text:SetPoint("CENTER")
+	BottomFrame.Left.Text:SetFont(cfg.Font, 9, "THINOUTLINE")
+	BottomFrame.Left.Text:SetText("G")
+	BottomFrame.Left.RaidMenuFrame = CreateFrame("Frame", "RaidMenu", UIParent, "UIDropDownMenuTemplate")
+	BottomFrame.Left:SetScript("OnMouseDown", function(self, button, ...)
+		if button == "RightButton" then
+			EasyMenu({
+				{text = "就位确认", func = function() DoReadyCheck() end}, 
+				{text = "角色检查", func = function() InitiateRolePoll() end}, 
+				{text = "转化为团队", func = function() ConvertToRaid() end}, 
+				{text = "转化为小队", func = function() ConvertToParty() end}}, 
+			self.RaidMenuFrame, "cursor", 0, 0, "MENU", 2)
 		end
 		PlaySound("igMiniMapOpen")
-end)
-BRFrame.Text = BRFrame:CreateFontString(nil, "OVERLAY")
-BRFrame.Text:SetPoint("CENTER", BRFrame, "CENTER", 0, 0)
-BRFrame.Text:SetFont(cfg.Font, 9, "THINOUTLINE")
-BRFrame.Text:SetText("B")
+	end)
+	DropDownList1:SetClampedToScreen(true)
+end
 
--- Updater
-local Timer = 0
-local Updater = CreateFrame("Frame")
-Updater:SetScript("OnUpdate", function(self, elapsed)
-    Timer = Timer + elapsed
-    if Timer > 1 then
-        Timer = 0
-		
-		-- 区域信息
-		local zone = GetMinimapZoneText()		
-        TMFrame.Text:SetText(zone)
-		
-		-- 坐标
-		local x, y = GetPlayerMapPosition("player")
-		BMFrame.Text:SetText(format("|　%.2d  :  %.2d　|", 100*x, 100*y))
-		
-		-- NBB
-		if NBB:IsVisible() then
-			NBBVisible = true
-		else
-			NBBVisible = false
+-- BuildExpBar
+local function BuildExpBar(BottomFrame)
+	local function numberize(value)
+		if value <= 9999 then return value end
+		if value >= 1000000 then
+			local value = string.format("%.1fm", value/1000000)
+			return value
+		elseif value >= 10000 then
+			local value = string.format("%.1fk", value/1000)
+			return value
 		end
+	end
 
-    end
+	local Bar = CreateFrame("StatusBar", nil, BottomFrame)
+	Bar:SetStatusBarTexture(cfg.Statusbar)
+	Bar:SetWidth(Minimap:GetWidth()-2)
+	Bar:SetHeight(4)
+	Bar:SetValue(0)
+	Bar:SetFrameStrata("MEDIUM")
+	Bar:SetPoint("TOP", BottomFrame, "BOTTOM", 0, -3)
+	Bar.Text = Bar:CreateFontString(nil, "OVERLAY")
+	Bar.Text:SetPoint("TOP", Bar, "BOTTOM", 0, -5)
+	Bar.Text:SetFont(cfg.Font, 9, "THINOUTLINE")
+	Bar.Text:SetAlpha(0)
+	Bar:SetScript("OnEnter", function()
+		BottomFrame:SetAlpha(1)
+		Bar.Text:SetAlpha(1)
+	end)
+	Bar:SetScript("OnLeave", function()
+		BottomFrame:SetAlpha(0.2)
+		Bar.Text:SetAlpha(0)
+	end)
+	Bar.R = CreateFrame("StatusBar", nil, BottomFrame)
+	Bar.R:SetStatusBarTexture(cfg.Statusbar)
+	Bar.R:SetAllPoints(Bar)
+	Bar.R:SetValue(0)
+	Bar.R:EnableMouse(true)
+	Bar.R:SetBackdrop({
+		bgFile= cfg.Statusbar, 
+		insets = {left = -1, right = -1, top = -1, bottom = -1}
+	})
+	Bar.R:SetBackdropColor(0, 0, 0, 1)
+	Bar.R.BG = Bar.R:CreateTexture(nil, "BORDER")
+	Bar.R.BG:SetAllPoints(Bar.R)
+	Bar.R.BG:SetTexture(cfg.Statusbar)
+	Bar.R.BG:SetVertexColor(0.16, 0.16, 0.16, 1)
+	
+	Bar:RegisterEvent("PLAYER_XP_UPDATE")
+	Bar:RegisterEvent("PLAYER_LEVEL_UP")
+	Bar:RegisterEvent("UPDATE_EXHAUSTION")
+	Bar:RegisterEvent("UPDATE_FACTION")
+	Bar:SetScript("OnEvent", function(self)
+		currXP = UnitXP("player")
+		playerMaxXP = UnitXPMax("player")
+		exhaustionXP  = GetXPExhaustion("player")
+		name, standing, minrep, maxrep, value = GetWatchedFactionInfo()
+		self.R:SetMinMaxValues(0, playerMaxXP)
+		self:SetMinMaxValues(0, playerMaxXP)
+		if UnitLevel("player") == MAX_PLAYER_LEVEL or IsXPUserDisabled == true then
+			if name then
+				self:SetStatusBarColor(0.9, 0.7, 0, 1)
+				self:SetMinMaxValues(minrep, maxrep)
+				self:SetValue(value)
+				self.R:SetValue(0)
+				self.Text:SetText(value-minrep.." / "..maxrep-minrep.."\n"..floor(((value-minrep)/(maxrep-minrep))*1000)/10 .."% | ".. name)
+				self:Show()
+				self.R:Show()
+			else
+				self:Hide()
+				self.R:Hide()
+			end
+		else
+			self:SetStatusBarColor(.4, .2, .8, 1)
+			self:SetValue(currXP)
+			if exhaustionXP  then
+				self.Text:SetText(numberize(currXP).." / "..numberize(playerMaxXP).."\n"..floor((currXP/playerMaxXP)*1000)/10 .."%" .. " (+"..numberize(exhaustionXP )..")")
+				self.R:SetMinMaxValues(0, playerMaxXP)
+				self.R:SetStatusBarColor(0.2, 0.4, 0.8, 0.3)
+				self:SetStatusBarColor(0.2, 0.4, 0.8, 1)
+				if (exhaustionXP +currXP) >= playerMaxXP then
+					self.R:SetValue(playerMaxXP)
+				else
+					self.R:SetValue(exhaustionXP +currXP)
+				end
+			else
+				self.Text:SetText(numberize(currXP).." / "..numberize(playerMaxXP).." | "..floor((currXP/playerMaxXP)*1000)/10 .."%")
+				self.R:SetValue(0)
+				self:SetStatusBarColor(0.4, 0.1, 0.6, 1)
+			end
+		end
+	end)
+end
+
+-- BuildBottomFrame
+local function BuildBottomFrame()
+	local BottomFrame = CreateFrame("Frame", nil, Minimap)
+	BottomFrame:SetHeight(16)
+	BottomFrame:SetWidth(Minimap:GetWidth())
+	BottomFrame:SetPoint("TOP", Minimap, "BOTTOM", 0, -3)
+	BottomFrame:SetBackdrop({edgeFile = cfg.Solid, edgeSize = 1})
+	BottomFrame:SetBackdropBorderColor(0, 0, 0, 1)
+	BottomFrame:SetAlpha(0.2)
+	BottomFrame:SetScript("OnEnter", function(self)
+		self:SetAlpha(1)
+	end)
+	BottomFrame:SetScript("OnLeave", function(self)
+		self:SetAlpha(0.2)
+	end)
+	BottomFrame.Text = BottomFrame:CreateFontString(nil, "OVERLAY")
+	BottomFrame.Text:SetPoint("CENTER", BottomFrame, "CENTER", 0, 0)
+	BottomFrame.Text:SetFont(cfg.Font, 10, "THINOUTLINE")
+	local x, y = 0, 0
+	BottomFrame.Timer = 0
+	BottomFrame:SetScript("OnUpdate", function(self, elapsed)
+		self.Timer = self.Timer + elapsed
+		if self.Timer > 1 then
+			self.Timer = 0
+			x, y = GetPlayerMapPosition("player")
+			self.Text:SetText(format("|　%.2d  :  %.2d　|", 100*x, 100*y))
+		end
+	end)
+	
+	BuildBottomRightFrame(BottomFrame)
+	BuildBottomLeftFrame(BottomFrame)
+	BuildExpBar(BottomFrame)
+end
+
+-- Event
+Event = CreateFrame("Frame")
+Event:RegisterEvent("PLAYER_LOGIN")
+Event:SetScript("OnEvent", function(self, event, ...)
+	if event == "PLAYER_LOGIN" then
+		BuildTopFrame()
+		BuildBottomFrame()
+	end
 end)
-
 
 
 
--- Sora's MiniMap/cfg.lua
+++ Sora's MiniMap/cfg.lua
@@ -7,11 +7,11 @@
 ------------
 
 local cfg = CreateFrame("Frame")
-local Media = "Interface\\Addons\\Sora's MiniMap\\media\\"
+local Media = "Interface\\Addons\\Sora's MiniMap\\Media\\"
 cfg.Font = GetLocale() == "zhCN" and "Fonts\\ZYKai_T.ttf" or "Fonts\\bLEI00D.ttf"
-cfg.GlowTex = Media.."glowTex"
-cfg.Solid = Media.."solid"
-cfg.Statusbar = Media.."statusbar"
+cfg.GlowTex = Media.."GlowTex"
+cfg.Solid = Media.."Solid"
+cfg.Statusbar = Media.."Statusbar"
 
 cfg.MinimapPos = {"TOPLEFT", UIParent, "TOPLEFT", 10, -30}	-- 小地图位置
 	
@@ -19,5 +19,5 @@ cfg.MinimapPos = {"TOPLEFT", UIParent, "TOPLEFT", 10, -30}	-- 小地图位置
 --  命名空间  --
 ----------------
 
-local _, SR = ...
-SR.MapConfig = cfg
+local _, ns = ...
+ns.cfg = cfg
