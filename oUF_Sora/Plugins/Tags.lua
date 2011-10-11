b41169b1710fef9e7e2c9c10594aa0d64a713251
Neavo <neavo7@gmail.com>
2011-10-11 12:10:12 +0800

    语法优化

--- oUF_Sora/Plugins/Tags.lua
+++ oUF_Sora/Plugins/Tags.lua
@@ -2,18 +2,13 @@
 local oUF = ns.oUF or oUF
 local cfg = ns.cfg
 
-oUF.colors.power['MANA'] = {0.1,0.3,1}
-
-local function IsInCombat()
-	Temp = UnitAffectingCombat("player")
-	return Temp
-end
+oUF.colors.power["MANA"] = {0.1, 0.3, 1}
 
 local SVal = function(val)
     if val >= 1e6 then
-        return ('%.1fm'):format(val / 1e6):gsub('%.?0+([km])$', '%1')
+        return ("%.1fm"):format(val / 1e6):gsub("%.?0+([km])$", "%1")
     elseif val >= 1e4 then
-        return ('%.1fk'):format(val / 1e3):gsub('%.?0+([km])$', '%1')
+        return ("%.1fk"):format(val / 1e3):gsub("%.?0+([km])$", "%1")
     else
         return val
     end
@@ -21,15 +16,15 @@ end
 
 local function hex(r, g, b)
 	if r then
-		if type(r) == 'table' then
+		if type(r) == "table" then
 			if(r.r) then r, g, b = r.r, r.g, r.b else r, g, b = unpack(r) end
 		end
-		return ('|cff%02x%02x%02x'):format(r * 255, g * 255, b * 255)
+		return ("|cff%02x%02x%02x"):format(r * 255, g * 255, b * 255)
 	end
 end
 
-oUF.Tags['Sora:hp']  = function(u) 
-	local per = oUF.Tags['perhp'](u).."%" or 0
+oUF.Tags["Sora:hp"]  = function(u) 
+	local per = oUF.Tags["perhp"](u).."%" or 0
 	local min, max = UnitHealth(u), UnitHealthMax(u)
 	if u == "target" or u == "focus" then
 		return SVal(min) .." | ".. per
@@ -39,9 +34,9 @@ oUF.Tags['Sora:hp']  = function(u)
 		return per
 	end
 end
-oUF.TagEvents['Sora:hp'] = 'UNIT_HEALTH'
+oUF.TagEvents["Sora:hp"] = "UNIT_HEALTH"
 
-oUF.Tags['Sora:pp'] = function(u)
+oUF.Tags["Sora:pp"] = function(u)
     local _, str = UnitPowerType(u)
     local power = UnitPower(u)
 	local powerMax = UnitPowerMax(u)
@@ -53,9 +48,9 @@ oUF.Tags['Sora:pp'] = function(u)
 		end
 	end
 end
-oUF.TagEvents['Sora:pp'] = 'UNIT_POWER'
+oUF.TagEvents["Sora:pp"] = "UNIT_POWER"
 
-oUF.Tags['Sora:color'] = function(u, r)
+oUF.Tags["Sora:color"] = function(u, r)
 	local _, class = UnitClass(u)
 	local reaction = UnitReaction(u, "player")
 	
@@ -71,7 +66,7 @@ oUF.Tags['Sora:color'] = function(u, r)
 		return hex(1, 1, 1)
 	end
 end
-oUF.TagEvents['Sora:color'] = 'UNIT_REACTION UNIT_HEALTH UNIT_HAPPINESS'
+oUF.TagEvents["Sora:color"] = "UNIT_REACTION UNIT_HEALTH UNIT_HAPPINESS"
 
 oUF.Tags["Sora:level"] = function(unit)
 	
@@ -84,23 +79,23 @@ oUF.Tags["Sora:level"] = function(unit)
 	if l <= 0 then l = "??" end
 	
 	if c == "worldboss" then
-		str = string.format("|cff%02x%02x%02xBoss|r",250,20,0)
+		str = string.format("|cff%02x%02x%02xBoss|r", 250, 20, 0)
 	elseif c == "eliterare" then
-		str = string.format("|cff%02x%02x%02x%s|r|cff0080FFR|r+",d.r*255,d.g*255,d.b*255,l)
+		str = string.format("|cff%02x%02x%02x%s|r|cff0080FFR|r+", d.r*255, d.g*255, d.b*255, l)
 	elseif c == "elite" then
-		str = string.format("|cff%02x%02x%02x%s|r+",d.r*255,d.g*255,d.b*255,l)
+		str = string.format("|cff%02x%02x%02x%s|r+", d.r*255, d.g*255, d.b*255, l)
 	elseif c == "rare" then
-		str = string.format("|cff%02x%02x%02x%s|r|cff0080FFR|r",d.r*255,d.g*255,d.b*255,l)
+		str = string.format("|cff%02x%02x%02x%s|r|cff0080FFR|r", d.r*255, d.g*255, d.b*255, l)
 	else
 		if not UnitIsConnected(unit) then
 			str = "??"
 		else
 			if UnitIsPlayer(unit) then
-				str = string.format("|cff%02x%02x%02x%s",d.r*255,d.g*255,d.b*255,l)
+				str = string.format("|cff%02x%02x%02x%s", d.r*255, d.g*255, d.b*255, l)
 			elseif UnitPlayerControlled(unit) then
-				str = string.format("|cff%02x%02x%02x%s",d.r*255,d.g*255,d.b*255,l)
+				str = string.format("|cff%02x%02x%02x%s", d.r*255, d.g*255, d.b*255, l)
 			else
-				str = string.format("|cff%02x%02x%02x%s",d.r*255,d.g*255,d.b*255,l)
+				str = string.format("|cff%02x%02x%02x%s", d.r*255, d.g*255, d.b*255, l)
 			end
 		end		
 	end
@@ -109,9 +104,8 @@ oUF.Tags["Sora:level"] = function(unit)
 end
 oUF.TagEvents["Sora:level"] = "UNIT_LEVEL PLAYER_LEVEL_UP UNIT_CLASSIFICATION_CHANGED"
 
-oUF.Tags['Sora:info'] = function(u)
+oUF.Tags["Sora:info"] = function(u)
     local _, class = UnitClass(u)
-
     if class then
         if UnitIsDead(u) then
             return hex(oUF.colors.class[class]).."死亡|r"
@@ -122,7 +116,7 @@ oUF.Tags['Sora:info'] = function(u)
         end
     end
 end
-oUF.TagEvents['Sora:info'] = 'UNIT_HEALTH UNIT_CONNECTION'
+oUF.TagEvents["Sora:info"] = "UNIT_HEALTH UNIT_CONNECTION"
 
 oUF.UnitlessTagEvents.PLAYER_REGEN_DISABLED = true
 oUF.UnitlessTagEvents.PLAYER_REGEN_ENABLED = true
