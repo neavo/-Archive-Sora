f1b626a5905aba33e5a5e2e8c82f8593d040c3ae
Neavo <neavo7@gmail.com>
2011-10-12 19:09:36 +0800

    AuraWatch默认样式更新

--- Sora's AuraWatch/Style.lua
+++ Sora's AuraWatch/Style.lua
@@ -7,6 +7,16 @@ local cfg = SR.AuraWatchConfig
 local class = select(2, UnitClass("player")) 
 local CLASS_COLORS = RAID_CLASS_COLORS[class]
 
+local function MakeShadow(Frame, Size)
+	local Shadow = CreateFrame("Frame", nil, Frame)
+	Shadow:SetFrameLevel(0)
+	Shadow:SetPoint("TOPLEFT", -Size, Size)
+	Shadow:SetPoint("BOTTOMRIGHT", Size, -Size)
+	Shadow:SetBackdrop({edgeFile = cfg.GlowTex, edgeSize = Size})
+	Shadow:SetBackdropBorderColor(0, 0, 0, 1)
+	return Shadow
+end
+
 -- BuildICON
 function cfg.BuildICON(iconSize)
 	local Frame = CreateFrame("Frame", nil, UIParent)
@@ -17,14 +27,7 @@ function cfg.BuildICON(iconSize)
 	Frame.Icon:SetAllPoints()
 	Frame.Icon:SetTexCoord(0.08, 0.92, 0.08, 0.92) 
 	
-	Frame.Shadow = CreateFrame("Frame", nil, Frame)
-	Frame.Shadow:SetPoint("TOPLEFT", -3, 3)
-	Frame.Shadow:SetPoint("BOTTOMRIGHT", 3, -3)
-	Frame.Shadow:SetBackdrop({ 
-		edgeFile = cfg.GlowTex , edgeSize = 3, 
-	})
-	Frame.Shadow:SetBackdropBorderColor(0, 0, 0, 1)
-	Frame.Shadow:SetFrameLevel(0)
+	Frame.Shadow = MakeShadow(Frame, 3)
 
 	Frame.Count = Frame:CreateFontString(nil, "OVERLAY") 
 	Frame.Count:SetFont(cfg.Font, 10, "THINOUTLINE") 
@@ -46,40 +49,39 @@ function cfg.BuildBAR(iconSize, barWidth)
 	Frame.Icon = Frame:CreateTexture(nil, "ARTWORK") 
 	Frame.Icon:SetWidth(iconSize)
 	Frame.Icon:SetHeight(iconSize)
-	Frame.Icon:SetPoint("RIGHT", Frame, "LEFT", 0, 0)
+	Frame.Icon:SetPoint("BOTTOMRIGHT", Frame, "BOTTOMLEFT", -5, 0)
+	
+	Frame.Icon.Shadow = MakeShadow(Frame, 3)
+	Frame.Icon.Shadow:SetPoint("TOPLEFT", Frame.Icon, -3, 3)
+	Frame.Icon.Shadow:SetPoint("BOTTOMRIGHT", Frame.Icon, 3, -3)
 
-	Frame.Statusbar = CreateFrame("StatusBar", nil, Frame) 
-	Frame.Statusbar:SetAllPoints() 
+	Frame.Statusbar = CreateFrame("StatusBar", nil, Frame)
+	Frame.Statusbar:SetWidth(Frame:GetWidth())
+	Frame.Statusbar:SetHeight(Frame:GetHeight()/3)	
+	Frame.Statusbar:SetPoint("BOTTOM") 
 	Frame.Statusbar:SetStatusBarTexture(cfg.Statusbar) 
 	Frame.Statusbar:SetStatusBarColor(CLASS_COLORS.r, CLASS_COLORS.g, CLASS_COLORS.b, 0.9)
 	Frame.Statusbar:SetMinMaxValues(0, 1) 
 	Frame.Statusbar:SetValue(0) 	
 
-	Frame.Statusbar.Shadow = CreateFrame("Frame", nil, Frame.Statusbar)
-	Frame.Statusbar.Shadow:SetPoint("TOPLEFT", -(3+iconSize), 3)
-	Frame.Statusbar.Shadow:SetPoint("BOTTOMRIGHT", 3, -3)
-	Frame.Statusbar.Shadow:SetBackdrop({ 
-		edgeFile = cfg.GlowTex , edgeSize = 3, 
-	})
-	Frame.Statusbar.Shadow:SetBackdropBorderColor(0, 0, 0, 1)
-	Frame.Statusbar.Shadow:SetFrameLevel(0)
+	Frame.Statusbar.Shadow = MakeShadow(Frame.Statusbar, 3)
 
 	Frame.Statusbar.BG = Frame.Statusbar:CreateTexture(nil, "BACKGROUND")
 	Frame.Statusbar.BG:SetAllPoints() 
 	Frame.Statusbar.BG:SetTexture(cfg.Statusbar)
-	Frame.Statusbar.BG:SetVertexColor(1, 1, 1, 0.2) 
+	Frame.Statusbar.BG:SetVertexColor(0.1, 0.1, 0.1, 0.2) 
 	
 	Frame.Count = Frame:CreateFontString(nil, "OVERLAY") 
 	Frame.Count:SetFont(cfg.Font, 9, "THINOUTLINE") 
 	Frame.Count:SetPoint("BOTTOMRIGHT", Frame.Icon, "BOTTOMRIGHT", 3, -1) 
 
 	Frame.Time = Frame.Statusbar:CreateFontString(nil, "ARTWORK") 
-	Frame.Time:SetFont(cfg.Font, 10, "THINOUTLINE") 
-	Frame.Time:SetPoint("RIGHT", 0, 0) 
+	Frame.Time:SetFont(cfg.Font, 11, "THINOUTLINE") 
+	Frame.Time:SetPoint("RIGHT", 0, 5) 
 
 	Frame.Spellname = Frame.Statusbar:CreateFontString(nil, "ARTWORK") 
-	Frame.Spellname:SetFont(cfg.Font, 10, "THINOUTLINE") 
-	Frame.Spellname:SetPoint("CENTER", -10, 1) 
+	Frame.Spellname:SetFont(cfg.Font, 11, "THINOUTLINE") 
+	Frame.Spellname:SetPoint("CENTER", -10, 5) 
 	
 	return Frame
 end
