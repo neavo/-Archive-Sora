48fc19ba0b5d36b6c8ca9512b8c6d4b9dbbb3170
Neavo <neavo7@gmail.com>
2011-10-14 02:44:56 +0800

    样式修改

--- Sora's_Config/Core.lua
+++ Sora's_Config/Core.lua
@@ -2,12 +2,6 @@
 local S, C, _, _ = unpack(select(2, ...))
 local SoraConfig = LibStub("AceAddon-3.0"):NewAddon("SoraConfig", "AceConsole-3.0")
 
--- ShowConfig
-local function ShowConfig()
-	LibStub("AceConfigDialog-3.0"):SetDefaultSize("Sora's Config", 780, 500)
-	LibStub("AceConfigDialog-3.0"):Open("Sora's Config")
-end
-
 -- ResetToDefault
 local function ResetToDefault()
 	for _, value in pairs(C) do
@@ -26,17 +20,37 @@ local function OnFristLoad()
 end
 
 Modules = {
+	ResetToDefault = {
+		type = "execute",
+		name = "恢复默认设置",
+		order = 1,
+		func = function() ResetToDefault() end
+	},
+	Hiden_1 = {
+		type = "execute",
+		name = " ",
+		order = 2,
+		disabled  = true,
+	},
+	Hiden_2 = {
+		type = "execute",
+		name = " ",
+		order = 3,
+		disabled  = true,
+	},
 	Reload = {
 		type = "execute",
 		name = "重载界面",
+		order = 4,
 		func = function() ReloadUI() end
 	},
-	Re = {
-		type = "execute",
-		name = "恢复默认设置",
-		func = function() ResetToDefault() end
-	}
 }
+
+-- ShowConfig
+local function ShowConfig()
+	LibStub("AceConfigDialog-3.0"):SetDefaultSize("Sora's Config", 740, 500)
+	LibStub("AceConfigDialog-3.0"):Open("Sora's Config")
+end
 
 -- OnInitialize
 function SoraConfig:OnInitialize()
