56d907779312bc2e2d325d5ab8b7253558e11273
Neavo <neavo7@gmail.com>
2011-10-17 04:12:37 +0800

    修正了鼠标提示框体战斗中指向物品时的BUG

--- Sora's/Modules/Tooltip/Tooltip.lua
+++ Sora's/Modules/Tooltip/Tooltip.lua
@@ -1,18 +1,10 @@
 ﻿-- Engines
 local _, ns = ...
-local _, _, _, DB = unpack(select(2, ...))
+local S, _, _, DB = unpack(select(2, ...))
 
-local classification = {
-    elite = "精英",
-    rare = "稀有",
-    rareelite = "稀有精英",
-}
+local classification = {elite = "精英", rare = "稀有", rareelite = "稀有精英"}
 
-local find = string.find
-local format = string.format
-local hex = function(color)
-    return format('|cff%02x%02x%02x', color.r * 255, color.g * 255, color.b * 255)
-end
+local tooltips = {GameTooltip, ItemRefTooltip, ShoppingTooltip1, ShoppingTooltip2, ShoppingTooltip3, WorldMapTooltip, DropDownList1MenuBackdrop, DropDownList2MenuBackdrop,}
 
 local function unitColor(unit)
     local color = { r=1, g=1, b=1 }
@@ -39,7 +31,7 @@ local function getTarget(unit)
     if UnitIsUnit(unit, "player") then
         return ("|cffff0000%s|r"):format("你")
     else
-        return hex(unitColor(unit))..UnitName(unit).."|r"
+        return S.ToHex(unitColor(unit))..UnitName(unit).."|r"
     end
 end
 
@@ -47,46 +39,29 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
     local name, unit = self:GetUnit()
 
     if unit then
-        if TooltipDB.HideInCombat and InCombatLockdown() then
-            return self:Hide()
-        end
+        if TooltipDB.HideInCombat and InCombatLockdown() then return self:Hide() end
 
         local color = unitColor(unit)
         local ricon = GetRaidTargetIndex(unit)
-
-        if ricon then
-            local text = GameTooltipTextLeft1:GetText()
-            GameTooltipTextLeft1:SetText(("%s %s"):format(ICON_LIST[ricon].."18|t", text))
-        end
 
         if UnitIsPlayer(unit) then
             self:AppendText((" |cff00cc00%s|r"):format(UnitIsAFK(unit) and CHAT_FLAG_AFK or 
             UnitIsDND(unit) and CHAT_FLAG_DND or 
             not UnitIsConnected(unit) and "<离线>" or ""))
-
             local unitGuild = GetGuildInfo(unit)
             local text2 = GameTooltipTextLeft2:GetText()
-            if unitGuild and text2 and text2:find("^"..unitGuild) then	
-                GameTooltipTextLeft2:SetTextColor(1, 0.1, 0.8)
-            end
+            if unitGuild and text2 and text2:find("^"..unitGuild) then GameTooltipTextLeft2:SetTextColor(1, 0.1, 0.8) end
         end
-
 
         local alive = not UnitIsDeadOrGhost(unit)
         local level = UnitLevel(unit)
-
         if level then
-            local unitClass = UnitIsPlayer(unit) and hex(color)..UnitClass(unit).."|r" or ""
+            local unitClass = UnitIsPlayer(unit) and S.ToHex(color)..UnitClass(unit).."|r" or ""
             local creature = not UnitIsPlayer(unit) and UnitCreatureType(unit) or ""
             local diff = GetQuestDifficultyColor(level)
-
-            if level == -1 then
-                level = "|cffff0000??"
-            end
-
+            if level == -1 then level = "|cffff0000首领" end
             local classify = UnitClassification(unit)
-            local textLevel = ("%s%s%s|r"):format(hex(diff), tostring(level), classification[classify] or "")
-
+            local textLevel = ("%s%s%s|r"):format(S.ToHex(diff), tostring(level), classification[classify] or "")
             for i=2, self:NumLines() do
                 local tiptext = _G["GameTooltipTextLeft"..i]
                 if tiptext:GetText():find(LEVEL) then
@@ -96,33 +71,21 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
                         tiptext:SetText(("%s %s"):format(textLevel, "|cffCCCCCC"..DEAD.."|r"):trim())
                     end
                 end
-
-                if tiptext:GetText():find(PVP) then
-                    tiptext:SetText(nil)
-                end
+                if tiptext:GetText():find(PVP) then tiptext:SetText(nil) end
             end
         end
-
-        if not alive then
-            GameTooltipStatusBar:Hide()
-        end
-
+        if not alive then GameTooltipStatusBar:Hide() end
         if UnitExists(unit.."target") then
             local tartext = ("%s: %s"):format(TARGET, getTarget(unit.."target"))
             self:AddLine(tartext)
         end
-
         GameTooltipStatusBar:SetStatusBarColor(color.r, color.g, color.b)
     else
         for i=2, self:NumLines() do
             local tiptext = _G["GameTooltipTextLeft"..i]
-
-            if tiptext:GetText():find(PVP) then
-                tiptext:SetText(nil)
-            end
+            if tiptext:GetText():find(PVP) then tiptext:SetText(nil) end
         end
-
-        GameTooltipStatusBar:SetStatusBarColor(0, .9, 0)
+        GameTooltipStatusBar:SetStatusBarColor(0, 0.9, 0)
     end
 
     if GameTooltipStatusBar:IsShown() then
@@ -130,53 +93,25 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 		GameTooltipStatusBar:SetHeight(6)
 		GameTooltipStatusBar:SetPoint("BOTTOMLEFT", GameTooltipStatusBar:GetParent(), "TOPLEFT", 1, 8)
 		GameTooltipStatusBar:SetPoint("BOTTOMRIGHT", GameTooltipStatusBar:GetParent(), "TOPRIGHT", -1, 8)
-		if not GameTooltipStatusBar.BG then 
-			GameTooltipStatusBar.BG = CreateFrame("Frame", nil, GameTooltipStatusBar)
-			GameTooltipStatusBar.BG:SetFrameLevel(GameTooltipStatusBar:GetFrameLevel()-1)
-			GameTooltipStatusBar.BG:SetPoint("TOPLEFT", -1, 1)
-			GameTooltipStatusBar.BG:SetPoint("BOTTOMRIGHT", 1, -1)
-			GameTooltipStatusBar.BG:SetBackdrop({
-				edgeFile = DB.Solid, edgeSize = 1,
-			})
-			GameTooltipStatusBar.BG:SetBackdropBorderColor(0, 0, 0, 1)
-		end
+		if not GameTooltipStatusBar.Shadow then GameTooltipStatusBar.Shadow = S.MakeShadow(GameTooltipStatusBar, 3) end
     end
 end)
 
 GameTooltipStatusBar:SetStatusBarTexture(DB.Statusbar)
-local bg = GameTooltipStatusBar:CreateTexture(nil, "BACKGROUND")
-bg:SetAllPoints(GameTooltipStatusBar)
-bg:SetTexture(DB.Statusbar)
-bg:SetVertexColor(0.5, 0.5, 0.5, 0.5)
-
-local numberize = function(val)
-    if val >= 1e6 then
-        return ("%.1fm"):format(val / 1e6)
-    elseif val >= 1e3 then
-        return ("%.1fk"):format(val / 1e3)
-    else
-        return ("%d"):format(val)
-    end
-end
-
 GameTooltipStatusBar:SetScript("OnValueChanged", function(self, value)
-    if not value then
-        return
-    end
+    if not value then return end
     local min, max = self:GetMinMaxValues()
-    if value < min or value > max then
-        return
-    end
+    if value < min or value > max then return end
     local _, unit = GameTooltip:GetUnit()
     if unit then
         min, max = UnitHealth(unit), UnitHealthMax(unit)
         if not self.text then
             self.text = self:CreateFontString(nil, "OVERLAY")
-			self.text:SetPoint("BOTTOM", GameTooltipStatusBar, "TOP", 0, -5)
+            self.text:SetPoint("CENTER", GameTooltipStatusBar)
             self.text:SetFont(DB.Font, 12, "THINOUTLINE")
         end
         self.text:Show()
-        local hp = numberize(min).." / "..numberize(max)
+        local hp = S.SVal(min).." / "..S.SVal(max)
         self.text:SetText(hp)
     end
 end)
@@ -193,53 +128,37 @@ local function SetTooltipPosition(tooltip)
 		tooltip:SetPoint(unpack(DB.TooltipPos))
 	end
 end
-
 hooksecurefunc("GameTooltip_SetDefaultAnchor", function(tooltip, parent)
-	if TooltipDB.HideInCombat and InCombatLockdown() then
-		tooltip:Hide()
+	if TooltipDB.Cursor then
+		tooltip:SetOwner(parent, "ANCHOR_CURSOR")
 	else
-		if TooltipDB.Cursor then
-			tooltip:SetOwner(parent, "ANCHOR_CURSOR")
-		else
-			tooltip:SetOwner(parent,"ANCHOR_NONE")
-		end
-		SetTooltipPosition(tooltip)
-		tooltip.default = 1
-		if not tooltip[tostring(tooltip)] then
-			tooltip[tostring(tooltip)] = 1
-			tooltip:HookScript("OnUpdate", function(self, ...)
-				if self.default then
-					SetTooltipPosition(self)
-				end
-			end)
-			tooltip:HookScript("OnHide", function(self, unit)
-				tooltip.default = nil
-				tooltip.unit = nil
-			end)
-		end
+		tooltip:SetOwner(parent,"ANCHOR_NONE")
+	end
+	SetTooltipPosition(tooltip)
+	tooltip.default = 1
+	if not tooltip[tostring(tooltip)] then
+		tooltip[tostring(tooltip)] = 1
+		tooltip:HookScript("OnUpdate", function(self, ...) if self.default then SetTooltipPosition(self) end end)
+		tooltip:HookScript("OnHide", function(self, unit)
+			tooltip.default = nil
+			tooltip.unit = nil
+		end)
 	end
 end)
 
 local function setBakdrop(frame)
-
 	frame:SetBackdrop({
-		bgFile = "Interface\\Buttons\\WHITE8x8", 
-		insets = { left = 2, right = 2, top = 2, bottom = 2 },
-		edgeFile = DB.Solid, edgeSize = 1,
+		bgFile = "Interface\\Buttons\\WHITE8x8", insets = {left = 3, right = 3, top = 3, bottom = 3},
+		edgeFile = DB.GlowTex, edgeSize = 3,
 	})
-	
     frame.freebBak = true
-	
 end
 
 local function style(frame)
-    if not frame.freebBak then
-        setBakdrop(frame)
-    end
-
+    if not frame.freebBak then setBakdrop(frame) end
     frame:SetBackdropColor(0.05, 0.05, 0.05, 0.4)
     frame:SetBackdropBorderColor(0, 0, 0, 1)
-	
+
     if frame.GetItem then
         local _, item = frame:GetItem()
         if item then
@@ -254,60 +173,50 @@ local function style(frame)
     end
 
 	local _, unit = GameTooltip:GetUnit()
-	if UnitIsPlayer(unit) then
-		frame:SetBackdropBorderColor(GameTooltip_UnitColor(unit))
-	end
+	if UnitIsPlayer(unit) then frame:SetBackdropBorderColor(GameTooltip_UnitColor(unit)) end
 
     if frame.NumLines then
         for index=1, frame:NumLines() do
             if index == 1 then
-                _G[frame:GetName()..'TextLeft'..index]:SetFont(DB.Font, 10+2, "THINOUTLINE")
+                _G[frame:GetName().."TextLeft"..index]:SetFont(DB.Font, 11+2, "THINOUTLINE")
             else
-                _G[frame:GetName()..'TextLeft'..index]:SetFont(DB.Font, 10, "THINOUTLINE")
+                _G[frame:GetName().."TextLeft"..index]:SetFont(DB.Font, 11, "THINOUTLINE")
             end
-            _G[frame:GetName()..'TextRight'..index]:SetFont(DB.Font, 10, "THINOUTLINE")
+            _G[frame:GetName().."TextRight"..index]:SetFont(DB.Font, 11, "THINOUTLINE")
         end
     end
 end
 
-local tooltips = {
-    GameTooltip,
-    ItemRefTooltip,
-    ShoppingTooltip1,
-    ShoppingTooltip2, 
-    ShoppingTooltip3,
-    WorldMapTooltip,
-    DropDownList1MenuBackdrop, 
-    DropDownList2MenuBackdrop,
-}
-
-for i, frame in ipairs(tooltips) do
-    frame:SetScript("OnShow", function(frame) style(frame) end)
-end
-
-local itemrefScripts = {
-    "OnTooltipSetItem",
-    "OnTooltipSetAchievement",
-    "OnTooltipSetQuest",
-    "OnTooltipSetSpell",
-}
-
-for i, script in ipairs(itemrefScripts) do
-    ItemRefTooltip:HookScript(script, function(self)
-        style(self)
+for _, value in ipairs(tooltips) do
+    value:SetScript("OnShow", function(value)
+        if TooltipDB.HideInCombat and InCombatLockdown() then return value:Hide() end
+        style(value) 
     end)
 end
 
-local f = CreateFrame"Frame"
-f:SetScript("OnEvent", function(self, event, ...) if ns[event] then return ns[event](ns, event, ...) end end)
-function ns:RegisterEvent(...) for i=1,select("#", ...) do f:RegisterEvent((select(i, ...))) end end
-function ns:UnregisterEvent(...) for i=1,select("#", ...) do f:UnregisterEvent((select(i, ...))) end end
-
-ns:RegisterEvent"PLAYER_LOGIN"
-function ns:PLAYER_LOGIN()
-    for i, frame in ipairs(tooltips) do
-        setBakdrop(frame)
-    end
-
-    ns:UnregisterEvent"PLAYER_LOGIN"
+for _, value in ipairs({"OnTooltipSetItem", "OnTooltipSetAchievement", "OnTooltipSetQuest", "OnTooltipSetSpell"}) do
+	ItemRefTooltip:HookScript(value, function(self) style(self) end)
 end
+
+-- Event
+local Event = CreateFrame("Frame")
+Event:SetScript("OnEvent", function(self, event, ...)
+	if ns[event] then return ns[event](ns, event, ...) end
+end)
+Event:RegisterEvent("PLAYER_LOGIN", function(self)
+	for i, frame in ipairs(tooltips) do
+		setBakdrop(frame)
+	end	
+end)
+function ns:RegisterEvent(...)
+	for i=1,select("#", ...) do
+		Event:RegisterEvent((select(i, ...)))
+	end
+end
+function ns:UnregisterEvent(...)
+	for i=1,select("#", ...) do
+		Event:UnregisterEvent((select(i, ...)))
+	end
+end
+
+
