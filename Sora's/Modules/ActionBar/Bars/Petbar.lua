0fe9eb30a740b38e2794c2b6a990aef801ac7699
Neavo <neavo7@gmail.com>
2011-10-24 02:38:43 +0800

    Update Petbar

--- Sora's/Modules/ActionBar/Bars/Petbar.lua
+++ Sora's/Modules/ActionBar/Bars/Petbar.lua
@@ -1,34 +1,27 @@
 ﻿-- Engines
-local S, _, _, DB = unpack(select(2, ...))
+local _, _, _, DB = unpack(select(2, ...))
 
-PetActionBarFrame:SetParent(DB.ActionBar)
-
-for i=1, NUM_PET_ACTION_SLOTS do
+for i = 1, NUM_PET_ACTION_SLOTS do
 	local Button = _G["PetActionButton"..i]
-	Button:SetSize(ActionBarDB.ActionBarButtonSize, ActionBarDB.ActionBarButtonSize)
 	Button:ClearAllPoints()
+	Button:SetParent(DB.ActionBar)
+	Button:SetAlpha(0.3)
+	Button:SetSize(ActionBarDB.ActionBarButtonSize, ActionBarDB.ActionBarButtonSize)
 	if i == 1 then
 		Button:SetPoint("BOTTOM", MultiBarBottomLeftButton6, "TOP", 0, 5)
 	else
-		local Pre = _G["PetActionButton"..i-1]      
-		Button:SetPoint("LEFT", Pre, "RIGHT", 3, 0)
+		Button:SetPoint("LEFT", _G["PetActionButton"..i-1], "RIGHT", 3, 0)
 	end
-	_G["PetActionButton"..i.."Cooldown"]:SetAllPoints(Button)
-end
-
-for i=1, NUM_PET_ACTION_SLOTS do
-	local Button = _G["PetActionButton"..i]
-	Button:SetAlpha(0.3)
 	Button:HookScript("OnEnter", function(self) 
 		for i = 1, NUM_PET_ACTION_SLOTS do
-			local Button = _G["PetActionButton"..i]
-			Button:SetAlpha(1)
+			_G["PetActionButton"..i]:SetAlpha(1)
 		end
 	end)
 	Button:HookScript("OnLeave",function(self) 
 		for i = 1, NUM_PET_ACTION_SLOTS do
-			local Button = _G["PetActionButton"..i]
-			Button:SetAlpha(0.3)
+			_G["PetActionButton"..i]:SetAlpha(0.3)
 		end
 	end)
-end 
+	RegisterStateDriver(Button, "visibility", "[pet,novehicleui,nobonusbar:5] show; hide")
+end
+PetActionBarFrame:ClearAllPoints()
