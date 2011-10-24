19930d607e47fc874c41876c65656c28735c3e38
Neavo <neavo7@gmail.com>
2011-10-25 02:35:08 +0800

    clean up

--- Sora's/Function/Variables.lua
+++ Sora's/Function/Variables.lua
@@ -1,5 +1,31 @@
 ﻿-- Engines
 local _, _, _, DB = unpack(select(2, ...))
+local Media = "InterFace\\AddOns\\Sora's\\Media\\"
 
 DB.MyClass = select(2, UnitClass("player"))
 DB.MyClassColor = RAID_CLASS_COLORS[DB.MyClass]
+DB.IsInCombat = UnitAffectingCombat("player")
+DB.Font = GetLocale() == "zhCN" and "Fonts\\ZYKai_T.ttf" or "Fonts\\bLEI00D.ttf"
+DB.bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"
+DB.Icon = "Interface\\Addons\\Sora's\\Modules\\Chat\\Icon\\"
+DB.GlowTex = Media.."GlowTex"
+DB.Statusbar = Media.."Statusbar"
+DB.ThreatBar = Media.."ThreatBar"
+DB.Solid = Media.."Solid"
+DB.Button = Media.."Button"
+DB.ArrowT = Media.."ArrowT"
+DB.Arrow = Media.."Arrow"
+DB.Warning = Media.."Warning.mp3"
+DB.RaidBuffPos = {"TOPLEFT", Minimap, "BOTTOMLEFT", -5, -25}
+DB.ClassBuffPos = {"CENTER", UIParent, -100, 150}
+DB.TooltipPos = {"BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -50, 201}
+DB.Colors = {
+	normal		= {r =   0, g =   0, b =    0},
+	pushed 		= {r =   1, g =   1, b =    1},
+	highlight 	= {r = 0.9, g = 0.8, b =  0.6},
+	checked 	= {r = 0.9, g = 0.8, b =  0.6},
+	outofrange 	= {r = 0.8, g = 0.3, b =  0.2},
+	outofmana 	= {r = 0.3, g = 0.3, b =  0.7},
+	usable 		= {r =   1, g =   1, b =    1},
+	unusable 	= {r = 0.4, g = 0.4, b =  0.4},
+}
