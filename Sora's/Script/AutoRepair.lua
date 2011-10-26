b7f0d150004453404fdc89044df235d2dd8b5de3
Neavo <neavo7@gmail.com>
2011-10-26 16:56:03 +0800

    update

--- Sora's/Script/AutoRepair.lua
+++ Sora's/Script/AutoRepair.lua
@@ -2,28 +2,6 @@
 local Event = CreateFrame("Frame")
 Event:RegisterEvent("MERCHANT_SHOW")
 Event:SetScript("OnEvent", function(self)
-
-	-- 自动出售垃圾
-	local c = 0
-	for b=0,4 do
-		for s=1,GetContainerNumSlots(b) do
-			local l = GetContainerItemLink(b, s)
-			if l then
-				local p = select(11, GetItemInfo(l))*select(2, GetContainerItemInfo(b, s))
-				if select(3, GetItemInfo(l))==0 and p>0 then
-					UseContainerItem(b, s)
-					PickupMerchantItem()
-					c = c+p
-				end
-			end
-		end
-	end
-	if c>0 then
-		local g, s, c = math.floor(c/10000) or 0, math.floor((c%10000)/100) or 0, c%100
-		DEFAULT_CHAT_FRAME:AddMessage("共售出：".." |cffffffff"..g.."|cffffc125 G|r".." |cffffffff"..s.."|cffc7c7cf S|r".." |cffffffff"..c.."|cffeda55f C|r"..".",255,255,255)
-	end
-	
-	-- 自动修理
 	if CanMerchantRepair() then
 		local cost, possible = GetRepairAllCost()
 		if cost>0 then
@@ -50,19 +28,3 @@ Event:SetScript("OnEvent", function(self)
 		end
 	end
 end)
-
--- 按住Alt一次购买一组
-local savedMerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedClick
-function MerchantItemButton_OnModifiedClick(self, ...)
-	if IsAltKeyDown() then
-		local itemLink = GetMerchantItemLink(self:GetID())
-		if not itemLink then
-			return
-		end
-		local maxStack = select(8, GetItemInfo(itemLink))
-		if maxStack and maxStack > 1 then
-			BuyMerchantItem(self:GetID(), GetMerchantItemMaxStack(self:GetID()))
-		end
-	end
-	savedMerchantItemButton_OnModifiedClick(self, ...)
-end
---
+++ Sora's/Script/AutoSell.lua
@@ -0,0 +1,23 @@
+﻿-- Event
+local Event = CreateFrame("Frame")
+Event:RegisterEvent("MERCHANT_SHOW")
+Event:SetScript("OnEvent", function(self)
+	local Cost = 0
+	for BagID = 0, 4 do
+		for SlotID = 1, GetContainerNumSlots(BagID) do
+			local Link = GetContainerItemLink(BagID, SlotID)
+			if Link then
+				local p = select(11, GetItemInfo(Link))*select(2, GetContainerItemInfo(b, SlotID))
+				if select(3, GetItemInfo(Link)) == 0 and p > 0 then
+					UseContainerItem(BagID, SlotID)
+					PickupMerchantItem()
+					Cost = Cost + p
+				end
+			end
+		end
+	end
+	if Cost > 0 then
+		local g, s, c = math.floor(c/10000) or 0, math.floor((c%10000)/100) or 0, c%100
+		DEFAULT_CHAT_FRAME:AddMessage("共售出：".." |cffffffff"..g.."|cffffc125 G|r".." |cffffffff"..s.."|cffc7c7cf S|r".." |cffffffff"..c.."|cffeda55f C|r"..".", 255, 255, 255)
+	end
+end)
