d466a058df506258afce4ad3f8ef50cec200cc83
Neavo <neavo7@gmail.com>
2011-11-03 03:22:32 +0800

    新的大地图美化模块

--- Sora's/Function/Variables.lua
+++ Sora's/Function/Variables.lua
@@ -4,7 +4,6 @@ local Media = "InterFace\\AddOns\\Sora's\\Media\\"
 
 DB.MyClass = select(2, UnitClass("player"))
 DB.MyClassColor = RAID_CLASS_COLORS[DB.MyClass]
-DB.IsInCombat = UnitAffectingCombat("player")
 DB.Font = GetLocale() == "zhCN" and "Fonts\\ZYKai_T.ttf" or "Fonts\\bLEI00D.ttf"
 DB.bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"
 DB.Icon = "Interface\\Addons\\Sora's\\Modules\\Chat\\Icon\\"
--- Sora's/Modules/Map/Map.lua
+++ Sora's/Modules/Map/Map.lua
@@ -1,108 +1,90 @@
--- Engines
+﻿-- Engines
 local S, _, _, DB = unpack(select(2, ...))
 
-local map_scale = 0.8
+WorldMapFrame.BG = CreateFrame("Frame", nil, WorldMapFrame)
+WorldMapFrame.BG:SetFrameLevel(0)
+WorldMapFrame.BG:SetPoint("TOPLEFT", -4, 4)
+WorldMapFrame.BG:SetPoint("BOTTOMRIGHT", 4, -4)
+WorldMapFrame.BG:SetBackdrop({
+	bgFile = DB.bgFile, insets = {left = 5, right = 5, top = 5, bottom = 5},
+	edgeFile = DB.GlowTex, edgeSize = 4
+})
+WorldMapFrame.BG:SetBackdropColor(0, 0, 0, 0.6)
+WorldMapFrame.BG:SetBackdropBorderColor(0, 0, 0, 1)
 
----------------- > Moving/removing world map elements
-if map_scale == 1 then map_scale = 0.99 end -- dirty hack to prevent "division by zero"!
-local Event = CreateFrame("Frame")
-Event:RegisterEvent("PLAYER_ENTERING_WORLD")
-Event:RegisterEvent("WORLD_MAP_UPDATE")
-Event:RegisterEvent("PLAYER_REGEN_DISABLED")
-Event:RegisterEvent("PLAYER_REGEN_ENABLED")
-Event:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_ENTERING_WORLD" then
-		SetCVar("questPOI", 1)
-		SetCVar("advancedWorldMap", 1)
-		BlackoutWorld:Hide()
-		BlackoutWorld.Show = function() end
-		BlackoutWorld.Hide = function() end
-		WORLDMAP_RATIO_MINI = map_scale
-		WORLDMAP_WINDOWED_SIZE = map_scale 
-		WORLDMAP_SETTINGS.size = map_scale 
-		WorldMapBlobFrame.Show = function() end
-		WorldMapBlobFrame.Hide = function() end
-		WorldMapQuestPOI_OnLeave = function() WorldMapTooltip:Hide() end
-		WorldMap_ToggleSizeDown()
-		if FeedbackUIMapTip then 
-			FeedbackUIMapTip:Hide()
-			FeedbackUIMapTip.Show = function() end
-		end
-	elseif event == "PLAYER_REGEN_DISABLED" then
+WorldMapZoomOutButton:SetPoint("LEFT", WorldMapZoneDropDown, "RIGHT", 0, 4)
+WorldMapLevelUpButton:SetPoint("TOPLEFT", WorldMapLevelDropDown, "TOPRIGHT", -2, 8)
+WorldMapLevelDownButton:SetPoint("BOTTOMLEFT", WorldMapLevelDropDown, "BOTTOMRIGHT", -2, 2)
+
+local function SmallSkin()
+	WorldMapFrame.BG:ClearAllPoints()
+	WorldMapFrame.BG:SetPoint("TOPLEFT", -4, 4)
+	WorldMapFrame.BG:SetPoint("BOTTOMRIGHT", 4, -4)
+	WorldMapLevelDropDown:ClearAllPoints()
+	WorldMapLevelDropDown:SetPoint("TOPLEFT", WorldMapDetailFrame, "TOPLEFT", -17, 27)
+end
+
+local function LargeSkin()
+	if not InCombatLockdown() then
+		WorldMapFrame:SetParent(UIParent)
+		WorldMapFrame:EnableMouse(false)
+		WorldMapFrame:EnableKeyboard(false)
+		SetUIPanelAttribute(WorldMapFrame, "area", "center")
+		SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
+	end
+	WorldMapFrame.BG:ClearAllPoints()
+	WorldMapFrame.BG:SetPoint("TOPLEFT", WorldMapDetailFrame, -25, 75)
+	WorldMapFrame.BG:SetPoint("BOTTOMRIGHT", WorldMapDetailFrame, 330, -240)  
+end
+
+local function QuestSkin()
+	if not InCombatLockdown() then
+		WorldMapFrame:SetParent(UIParent)
+		WorldMapFrame:EnableMouse(false)
+		WorldMapFrame:EnableKeyboard(false)
+		SetUIPanelAttribute(WorldMapFrame, "area", "center")
+		SetUIPanelAttribute(WorldMapFrame, "allowOtherPanels", true)
+	end
+	WorldMapFrame.BG:ClearAllPoints()
+	WorldMapFrame.BG:SetPoint("TOPLEFT", WorldMapDetailFrame, -25, 75)
+	WorldMapFrame.BG:SetPoint("BOTTOMRIGHT", WorldMapDetailFrame, 330, -240)  
+end			
+
+local function FixSkin()
+	for i = 1, WorldMapFrame:GetNumRegions() do
+		local region = select(i, WorldMapFrame:GetRegions())
+		if region:GetObjectType() == "Texture" then region:SetTexture(nil) end
+	end	
+	
+	if WORLDMAP_SETTINGS.size == WORLDMAP_FULLMAP_SIZE then
+		LargeSkin()
+	elseif WORLDMAP_SETTINGS.size == WORLDMAP_WINDOWED_SIZE then
+		SmallSkin()
+	elseif WORLDMAP_SETTINGS.size == WORLDMAP_QUESTLIST_SIZE then
+		QuestSkin()
+	end
+
+	if not InCombatLockdown() then
+		WorldMapFrameSizeDownButton:Show()
+		WorldMapFrame:SetFrameLevel(10)
+	else
+		WorldMapFrameSizeDownButton:Disable()
 		WorldMapFrameSizeUpButton:Disable()
-		WorldMap_ToggleSizeDown()
-		WorldMapBlobFrame:DrawBlob(WORLDMAP_SETTINGS.selectedQuestId, false)
-		WorldMapBlobFrame:DrawBlob(WORLDMAP_SETTINGS.selectedQuestId, true)
-	elseif event == "PLAYER_REGEN_ENABLED" then
-		WorldMapFrameSizeUpButton:Enable()
-	elseif event == "WORLD_MAP_UPDATE" then
-		if GetNumDungeonMapLevels() == 0 then
-			WorldMapLevelUpButton:Hide()
-			WorldMapLevelDownButton:Hide()
-		else
-			WorldMapLevelUpButton:Show()
-			WorldMapLevelUpButton:ClearAllPoints()
-			WorldMapLevelUpButton:SetPoint("TOPLEFT", WorldMapFrameCloseButton, "BOTTOMLEFT", 8, 8)
-			WorldMapLevelUpButton:SetFrameStrata("MEDIUM")
-			WorldMapLevelUpButton:SetFrameLevel(100)
-			WorldMapLevelUpButton:SetParent("WorldMapFrame")
-			WorldMapLevelDownButton:ClearAllPoints()
-			WorldMapLevelDownButton:Show()
-			WorldMapLevelDownButton:SetPoint("TOP", WorldMapLevelUpButton, "BOTTOM", 0, -2)
-			WorldMapLevelDownButton:SetFrameStrata("MEDIUM")
-			WorldMapLevelDownButton:SetFrameLevel(100)
-			WorldMapLevelDownButton:SetParent("WorldMapFrame")
-		end
+	end	
+end
+
+WorldMapFrame:HookScript("OnShow", FixSkin)
+hooksecurefunc("WorldMapFrame_SetFullMapView", LargeSkin)
+hooksecurefunc("WorldMapFrame_SetQuestMapView", QuestSkin)
+hooksecurefunc("WorldMap_ToggleSizeUp", FixSkin)
+
+WorldMapFrame:RegisterEvent("PLAYER_LOGIN")
+WorldMapFrame:HookScript("OnEvent", function(self, event)
+	if event == "PLAYER_LOGIN" then
+		if not GetCVarBool("miniWorldMap") then ToggleFrame(WorldMapFrame) end
 	end
 end)
 
----------------- > Styling mini World Map
 
-hooksecurefunc("WorldMap_ToggleSizeDown", function()
-	if not WorldMapButton.Shadow then WorldMapButton.Shadow = S.MakeShadow(WorldMapButton, 5) end
-	WorldMapFrame.scale = map_scale
-	WorldMapDetailFrame:SetScale(map_scale)
-	WorldMapButton:SetScale(map_scale)
-	WorldMapFrameAreaFrame:SetScale(map_scale)
-	WorldMapTitleButton:Show()
-	WorldMapFrameMiniBorderLeft:Hide()
-	WorldMapFrameMiniBorderRight:Hide()
-	WorldMapPOIFrame.ratio = map_scale
-	WorldMapFrameCloseButton:ClearAllPoints()
-	WorldMapFrameCloseButton:SetPoint("TOPRIGHT", WorldMapButton)
-	WorldMapFrameCloseButton:SetFrameStrata("HIGH")
-	WorldMapFrameCloseButton:SetScale(map_scale)
-	WorldMapFrameSizeUpButton:Show()
-	WorldMapFrameSizeUpButton:ClearAllPoints()
-	WorldMapFrameSizeUpButton:SetPoint("RIGHT", WorldMapFrameCloseButton, "LEFT", 10, 0)
-	WorldMapFrameSizeUpButton:SetFrameStrata("HIGH")
-	WorldMapFrameSizeUpButton:SetScale(map_scale)
-	WorldMapFrameTitle:Hide()
-	WorldMapTitleButton:SetFrameStrata("TOOLTIP")
-	WorldMapTitleButton:ClearAllPoints()
-	WorldMapTitleButton:SetPoint("BOTTOM", WorldMapDetailFrame, "TOP", 0, 0)
-	WorldMapTooltip:SetFrameStrata("TOOLTIP")
-	WorldMapLevelDropDown.Show = function() end
-	WorldMapLevelDropDown:Hide()
-	WorldMapQuestShowObjectives:SetScale(map_scale)
-	WorldMapShowDigSites:SetScale(map_scale)
-	WorldMapTrackQuest:SetScale(map_scale)
-	WorldMapLevelDownButton:SetScale(map_scale)
-	WorldMapLevelUpButton:SetScale(map_scale)
-	WorldMapFrame_SetOpacity(WORLDMAP_SETTINGS.opacity)
-	WorldMapQuestShowObjectives_AdjustPosition()
-end)
 
-hooksecurefunc("WorldMap_ToggleSizeUp", function()
-	WorldMapQuestShowObjectives:SetScale(1)
-	WorldMapTrackQuest:SetScale(1)
-	WorldMapFrameCloseButton:SetScale(1)
-	WorldMapShowDigSites:SetScale(1)
-	WorldMapLevelDownButton:SetScale(1)
-	WorldMapLevelUpButton:SetScale(1)
-	WorldMapLevelDropDown.Show = WorldMapLevelDropDown:Show()
-	WorldMapFrame:EnableKeyboard(nil)
-	WorldMapFrame:EnableMouse(nil)
-	UIPanelWindows["WorldMapFrame"].area = "center"
-	WorldMapFrame:SetAttribute("UIPanelLayout-defined", nil)
-end)
+
