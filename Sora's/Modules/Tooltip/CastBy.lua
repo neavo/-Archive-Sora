0d050aa4b98e968c934bc1304e87de53df97c78d
Neavo <neavo7@gmail.com>
2011-11-24 23:53:59 +0800

    更新 CastBy

--- Sora's/Modules/Tooltip/CastBy.lua
+++ Sora's/Modules/Tooltip/CastBy.lua
@@ -1,41 +1,74 @@
-﻿local function addAuraSource(self, func, unit, index, filter)
-   local caster = select(8, func(unit, index, filter))
-   if caster then
-      local colors = CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS
-      local castername = UnitName(caster)
-      local partypet, raidpet, color
-      if UnitIsPlayer(caster) then
-         color = colors[select(2,UnitClass(caster))]
-         castername = format("|cff%02X%02X%02X%s|r" , color.r*255, color.g*255, color.b*255, UnitName(caster))
-      else
-         if caster == "pet" or caster == "vehicle" then
-            color = colors[select(2,UnitClass("player"))]
-            castername = format("|cff%02X%02X%02X%s|r%s%s", color.r*255, color.g*255, color.b*255, UnitName("player"), "|cffC0C0C0的|r", UnitName(caster))
-         else
-            partypet = caster:match("^partypet(%d+)$")
-            raidpet = caster:match("^raidpet(%d+)$")
-            if partypet then
-               color = colors[select(2,UnitClass("party"..partypet))]
-               castername = format("|cff%02X%02X%02X%s|r%s%s" , color.r*255, color.g*255, color.b*255, UnitName("party"..partypet), "|cffC0C0C0的|r", UnitName(caster))
-            elseif raidpet then
-               color = colors[select(2,UnitClass("party"..raidpet))]
-               castername = format("|cff%02X%02X%02X%s|r%s%s" , color.r*255, color.g*255, color.b*255, UnitName("raid"..raidpet), "|cffC0C0C0的|r", UnitName(caster))
-            end
-         end
-      end
-      self:AddDoubleLine("Cast by：", castername)
-      self:Show()
-   end
+﻿local a, b, d = _G.GameTooltip.SetUnitAura, _G.GameTooltip.SetUnitBuff, _G.GameTooltip.SetUnitDebuff
+local un, uc, uvsi, ua, uip, upc, sub = _G.UnitName, _G.UnitClass, _G.UnitVehicleSeatInfo, _G.UnitAura, _G.UnitIsPlayer, _G.UnitPlayerControlled, _G.string.sub
+local co = setmetatable({}, {__index = function(t, cl)
+	local c = CUSTOM_CLASS_COLORS and CUSTOM_CLASS_COLORS[cl] or RAID_CLASS_COLORS[cl]
+	if c then
+		t[cl] = ("ff%02x%02x%02x"):format(c.r * 255, c.g * 255, c.b * 255)
+	else
+		t[cl] = "ffffffff"
+	end
+	return t[cl]
+end})
+
+local function h(o, ...)
+	o(...)
+	local _, uid, id, f = ...
+	if o == b then
+		f = "HELPFUL " .. (f or "")
+	elseif o == d then
+		f = "HARMFUL " .. (f or "")
+	end
+	local _, _, _, _, _, _, _, c = ua(uid, id, f)
+	local cl, str
+	if c then
+		if not uip(c) and upc(c) then
+			local n
+			_, n = uvsi(c, 1)
+			if n then
+				_, cl = uc(n)
+				str = ("|c%s%s|r"):format(co[cl], n)
+				_, n = uvsi(c, 2)
+				if n then
+					_, cl = uc(n)
+					str = str.." & "..("|c%s%s|r"):format(co[cl], n)
+				end
+			else
+				local cl2, n2
+				if c == "pet" then
+					_, cl=uc(c);_, cl2 = uc("player");n, n2 = un(c), un("player")
+				elseif sub(c, 1, 8) == "partypet" then
+					id = sub(c, 9)
+					_, cl = uc(c);_, cl2 = uc("party"..id);n, n2 = un(c), un("party"..id)
+				elseif sub(c, 1, 7) == "raidpet" then
+					id = sub(c, 8)
+					_, cl = uc(c);_, cl2=uc("raid"..id);n, n2 = un(c), un("raid"..id)
+				end
+				if cl then
+					str = ("|c%s%s|r (|c%s%s|r)"):format(co[cl], n,co[cl2], n2)
+				else
+					_, cl = uc(c)
+					str = ("|c%s%s|r"):format(co[cl], un(c))
+				end
+			end
+		else
+			_, cl = uc(c)
+			str = ("|c%s%s|r"):format(co[cl], un(c))
+		end
+	end
+	if str then
+		GameTooltip:AddDoubleLine(DONE_BY,str)
+		GameTooltip:Show()
+	end
 end
 
-local funcs = {
-   SetUnitAura = UnitAura,
-   SetUnitBuff = UnitBuff,
-   SetUnitDebuff = UnitDebuff,
-}
+GameTooltip.SetUnitAura = function( ... )
+	h( a, ... )
+end
 
-for k, v in pairs(funcs) do
-   hooksecurefunc(GameTooltip, k, function(self, unit, index, filter)
-      addAuraSource(self, v, unit, index, filter)
-   end)
+GameTooltip.SetUnitBuff = function( ... )
+	h( b, ... )
+end
+
+GameTooltip.SetUnitDebuff = function( ... )
+	h( d, ... )
 end
