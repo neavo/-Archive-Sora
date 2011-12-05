5cfae8dd5b0eacbe9355f6bf7cb0177f64ec92b3
Neavo <neavo7@gmail.com>
2011-12-05 17:00:22 +0800

    1.小地图右键菜单调整 2.新邮件提醒方式调整

--- Sora's/Modules/Map/MiniMap.lua
+++ Sora's/Modules/Map/MiniMap.lua
@@ -2,11 +2,11 @@
 local S, C, L, DB = unpack(select(2, ...))
 local Module = LibStub("AceAddon-3.0"):GetAddon("Sora"):NewModule("MiniMap")
 
-local NewMail = false
 Minimap:SetMaskTexture("Interface\\ChatFrame\\ChatFrameBackground")
 Minimap:SetFrameStrata("BACKGROUND")
 Minimap:ClearAllPoints()
 Minimap:SetSize(120, 120)
+Minimap.Shadow = S.MakeShadow(Minimap, 4)
 
 function Module:OnInitialize()
 	MoveHandle.Minimap = S.MakeMoveHandle(Minimap, "小地图", "Minimap")
@@ -16,22 +16,6 @@ if LFDSearchStatus then
 	LFDSearchStatus:SetClampedToScreen(true)
 end
 DropDownList1:SetClampedToScreen(true)
-
-Minimap.Shadow = S.MakeShadow(Minimap, 4)
-Minimap.Shadow.Timer = 0
-Minimap.Shadow:SetScript("OnUpdate",function(self,elapsed)
-	self.Timer = self.Timer + elapsed
-	if self.Timer > 1.2 then
-		self.Timer = 0
-		if HasNewMail() then
-			self:SetBackdropBorderColor(120/255, 255/255, 120/255, 1)
-			UIFrameFadeOut(self, 1.2, 1, 0)
-		else
-			self:SetAlpha(1)
-			self:SetBackdropBorderColor(0, 0, 0, 1)
-		end
-	end
-end)
 
 local frames = {
 	"GameTimeFrame",
@@ -55,41 +39,39 @@ MinimapCluster:EnableMouse(false)
 
 --BG icon
 MiniMapBattlefieldFrame:ClearAllPoints()
-MiniMapBattlefieldFrame:SetPoint("BOTTOMLEFT", Minimap, 0, 0)
+MiniMapBattlefieldFrame:SetPoint("BOTTOMLEFT", 0, -4)
 
 --LFG icon
 MiniMapLFGFrame:ClearAllPoints()
 MiniMapLFGFrameBorder:SetAlpha(0)
-MiniMapLFGFrame:SetPoint("TOPLEFT", Minimap, 0, 0)
+MiniMapLFGFrame:SetPoint("TOPLEFT", 0, 4)
 
 MiniMapInstanceDifficulty:ClearAllPoints()
-MiniMapInstanceDifficulty:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 0, 4)
+MiniMapInstanceDifficulty:SetPoint("TOPRIGHT", 0, 4)
 MiniMapInstanceDifficulty:SetScale(0.8)
 
 GuildInstanceDifficulty:ClearAllPoints()
-GuildInstanceDifficulty:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 0, 4)
+GuildInstanceDifficulty:SetPoint("TOPRIGHT", 0, 4)
 GuildInstanceDifficulty:SetScale(0.8)
 
 MiniMapMailFrame:ClearAllPoints()
-MiniMapMailFrame:SetPoint("TOPRIGHT", Minimap, 0, 0)
-MiniMapMailFrame:SetFrameStrata("LOW")
-MiniMapMailIcon:SetTexture("Interface\\AddOns\\Sora's\Media\\mail.tga")
+MiniMapMailFrame:SetPoint("BOTTOMRIGHT", 0, -4)
+MiniMapMailIcon:SetTexture("Interface\\AddOns\\Sora's\\Media\\Mail")
 MiniMapMailBorder:Hide()
 
 -- Enable mouse scrolling
 Minimap:EnableMouseWheel(true)
-Minimap:SetScript("OnMouseWheel", function(self, d)
-	if d > 0 then
-		_G.MinimapZoomIn:Click()
-	elseif d < 0 then
-		_G.MinimapZoomOut:Click()
+Minimap:SetScript("OnMouseWheel", function(self, z)
+	local c = Minimap:GetZoom()
+	if z > 0 and c < 5 then
+		Minimap:SetZoom(c+1)
+	elseif z < 0 and c > 0 then
+		Minimap:SetZoom(c-1)
 	end
 end)
 
 local menuFrame = CreateFrame("Frame", "MinimapRightClickMenu", UIParent, "UIDropDownMenuTemplate")
 local menuList = {
-	{text = ENCOUNTER_JOURNAL, 
-	func = function() if not IsAddOnLoaded("Blizzard_EncounterJournal") then LoadAddOn("Blizzard_EncounterJournal"); end ToggleFrame(EncounterJournal) end},	
 	{text = CHARACTER_BUTTON,
 	func = function() ToggleCharacter("PaperDollFrame") end},
 	{text = SPELLBOOK_ABILITIES_BUTTON,
@@ -100,17 +82,22 @@ local menuList = {
 	func = function() ToggleAchievementFrame() end},
 	{text = QUESTLOG_BUTTON,
 	func = function() ToggleFrame(QuestLogFrame) end},
-	{text = SOCIAL_BUTTON,
-	func = function() ToggleFriendsFrame(1) end},
+	{text = GUILD,
+	func = function() ToggleGuildFrame(1) end},
 	{text = PLAYER_V_PLAYER,
 	func = function() ToggleFrame(PVPFrame) end},
-	{text = LFG_TITLE,
+	{text = "地下城查找器",
 	func = function() ToggleFrame(LFDParentFrame) end},
-	{text = L_LFRAID,
-	func = function() ToggleFrame(LFRParentFrame) end},
+	{text = ENCOUNTER_JOURNAL, 
+	func = function() ToggleFrame(EncounterJournal) end},
+	{text = RAID, 
+	func = function() ToggleFriendsFrame(4) end},
 	{text = HELP_BUTTON,
 	func = function() ToggleHelpFrame() end},
-	{text = "打开背包", func = function() ToggleBackpack() end}, 
+	{text = "打开背包",
+	func = function() ToggleBackpack() end}, 
+	{text = L_LFRAID,
+	func = function() ToggleFrame(LFRParentFrame) end},
 }
 
 Minimap:SetScript("OnMouseUp", function(self, button)
