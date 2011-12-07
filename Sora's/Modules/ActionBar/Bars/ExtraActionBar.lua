7db018c85ec6dab9a51071934ecd014a7f470d13
Neavo <neavo7@gmail.com>
2011-12-07 16:29:38 +0800

    update

--- Sora's/Modules/ActionBar/Bars/ExtraActionBar.lua
+++ Sora's/Modules/ActionBar/Bars/ExtraActionBar.lua
@@ -7,7 +7,7 @@ if uiVersion ~= 40300 then return end
 
 function Module:OnEnable()
 
-	local Bar = CreateFrame("Frame", nil,UIParent, "SecureHandlerStateTemplate")
+	local Bar = CreateFrame("Frame", nil, UIParent, "SecureHandlerStateTemplate")
 	Bar:SetSize(40, 40)
 	Bar:SetPoint("BOTTOM", 0, 350)
 
